8253e509eeeb38741afe07df47babb58c70d4802
One Link Uganda <musiramandsons220@gmail.com>
2025-10-14 05:53:25 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -23,18 +23,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
-    ],
-  },
-  experimental: {
-    // This is to allow the Next.js dev server to accept requests from the
-    // Firebase Studio development environment.
-    allowedDevOrigins: [
-      'https://*.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev',
     ],
   },
 };
