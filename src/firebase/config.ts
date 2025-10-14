711ff078b8cef7c2683201dede5fd9410cb56781
One Link Uganda <musiramandsons220@gmail.com>
2025-10-14 05:27:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2999977485-a51ab",
+  "appId": "1:957350540199:web:583b4ad177dd05a2a5fc82",
+  "apiKey": "AIzaSyAMY9U0lM9uA2RcgeuHedqJ_y9Hk0uH7Ws",
+  "authDomain": "studio-2999977485-a51ab.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "957350540199"
+};
